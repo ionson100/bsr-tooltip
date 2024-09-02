8f4bb0c89e6186ad41f8741225e8a0301f9b7ba1
123 <123@DESKTOP-2UK3QN5>
2024-09-02 20:56:05 +0200

    +.2.2.51

---
+++ dist/cjs/tooltipCore/wrapper.d.ts
@@ -0,0 +1,3 @@
+import React, { ReactElement } from "react";
+import { Options } from "./innerClass";
+export declare const useToolTip: (target: React.RefObject<HTMLElement> | null, body: ReactElement | string | number, options?: Options) => void;
