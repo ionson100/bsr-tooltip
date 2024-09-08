b69700209f0fe4f91da863d0d6271e7a7f8901cb
123 <123@DESKTOP-2UK3QN5>
2024-09-08 14:33:31 +0200

    +2.2.65

--- dist/cjs/tooltipCore/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Options, TooltipProps, ToolTip } from './innerClass';
-import { useToolTip } from './wrapper';
-export type { Options, TooltipProps };
-export { useToolTip, ToolTip };
--- dist/esm/tooltipCore/wrapper.d.ts
+++
@@ -1,7 +0,0 @@
-import React, { ReactElement } from "react";
-import { Options, ToolTip } from "./innerClass";
-type res = {
-    tooltip?: ToolTip;
-};
-export declare const useToolTip: (target: React.RefObject<Element> | null, body: ReactElement | string | number, options?: Options) => res;
-export {};
